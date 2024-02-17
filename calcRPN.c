d016aa0c6aff273ebae666d775c0e1160482f3c2
Nestor Gutierrez <nestorg456k@outlook.com>
2024-02-16 21:53:06 -0500

    update calcRPN.c

--- calcRPN.c
+++ calcRPN.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-
 double stack[8];
 int top = -1;
 
@@ -44,6 +43,10 @@ double pop()
 
 void clear_stack()
 {
+    for (int i = 0; i < 8; i++)
+    {
+        stack[i] = 0.0;
+    }
     top = -1;
 }
 
@@ -73,15 +76,12 @@ void popFirst()
     }
 }
 
-
-int main()
-{
+int main() {
     char opcion;
-    double num;
+    double num1;
 
-    while (1)
-    {
-        printf("\033[2J"); // Limpiar la pantalla
+    while (1) {
+        
         printStack();
         printf("\nMenu:\n");
         printf("1. Agregar numero a la pila\n");
@@ -92,90 +92,84 @@ int main()
         printf("Seleccione una opcion: ");
         scanf(" %c", &opcion);
 
-        switch (opcion)
-        {
-        case '1':
-            printf("Ingrese un numero: ");
-            scanf("%lf", &num);
-            push(num);
-            break;
-        case '2':
-            printf("Ingrese la operacion (+, -, *, /, p (potencia), r (raiz), s (seno), c (coseno), t (tangente)) o 'q' para regresar al menu anterior: ");
-            scanf(" %c", &opcion);
-            if (opcion == 'q')
-            {
-                continue; // Regresar al menú anterior
-            }
-            else if (top >= 1)
-            {
-                double resultado;
-                double num1 = pop();
-                switch (opcion)
-                {
-                case '+':
-                    resultado = num1 + pop();
-                    break;
-                case '-':
-                    resultado = num1 - pop();
-                    break;
-                case '*':
-                    resultado = num1 * pop();
-                    break;
-                case '/':
-                    // Verificar division por cero
-                    double num2 = pop();
-                    if ( num2 == 0)
-                    {
-                        printf("No se puede dividir entre cero.\n");
-                        continue;
+        switch (opcion) {
+            case '1':
+                printf("Ingrese un numero: ");
+                scanf("%lf", &num1);
+                push(num1);
+                break;
+            case '2':
+                if (top >= 1) {
+                    printf("Ingrese la operacion (+, -, *, /, p (potencia), r (raiz), s (seno), c (coseno), t (tangente)): ");
+                    scanf(" %c", &opcion);
+                    double resultado;
+                    num1 = pop();
+                    switch (opcion) {
+                        case '+':
+                            resultado = num1 + pop();
+                            break;
+                        case '-':
+                            resultado = num1 - pop();
+                            break;
+                        case '*':
+                            resultado = num1 * pop();
+                            break;
+                        case '/':
+                            double num2 = pop();
+                            if (num2 == 0) {
+                                printf("No se puede dividir entre cero.\n");
+                                push(num2);
+                                push(num1); 
+                                continue;
+                            }
+                            resultado = num1 / num2;
+                            break;
+                        case 'p':
+                            resultado = pow(num1, pop());
+                            break;
+                        case 'r':
+                            if (num1 < 0) {
+                                printf("No se puede calcular la raiz cuadrada de un numero negativo.\n");
+                                push(num1); // Devuelve el valor a la pila
+                                continue;
+                            }
+                            resultado = sqrt(num1);
+                            break;
+                        case 's':
+                            resultado = sin(num1 * M_PI / 180);
+                            break;
+                        case 'c':
+                            resultado = cos(num1 * M_PI / 180);
+                            break;
+                        case 't':
+                            if (num1 == 90 || num1 == 270) {
+                                printf("No se puede calcular la tangente de 90 o 270 grados.\n");
+                                push(num1); // Devuelve el valor a la pila
+                                continue;
+                            }
+                            resultado = tan(num1 * M_PI / 180);
+                            break;
+                        default:
+                            printf("Operacion no valida.\n");
+                            push(num1); // Devuelve el valor a la pila
+                            continue;
                     }
-                    resultado = num1 / num2;
-                case 'p':
-                    resultado = pow(num1, pop());
-                    break;
-                case 'r':
-                    if(num1 < 0)
-                    {
-                        printf("No se puede calcular la raiz cuadrada de un numero negativo.\n");
-                        continue;
-                    }
-                    resultado = sqrt(num1);
-                case 's':
-                    resultado = sin(num1 * M_PI / 180);
-                    break;
-                case 'c':
-                    resultado = cos(num1 * M_PI / 180);
-                    break;
-                case 't':
-                    if (num1 == 90 || num1 == 270)
-                    {
-                        printf("No se puede calcular la tangente de 90 o 270 grados.\n");
-                        continue;
-                    }
-                    resultado = tan(num1 * M_PI / 180);                 
-                    break;
-                default:
-                    printf("Operacion no valida.\n");
-                    continue;
+                    push(resultado);
+                } else {
+                    printf("Se necesitan al menos dos numeros en la pila para realizar una operacion.\n");
                 }
-                push(resultado);
-            }
-            else
-            {
-                printf("Se necesitan al menos dos numeros en la pila para realizar una operacion.\n");
-            }
-            break;
-        case '3':
-            clear_stack();
-            break;
-        case '4':
-            popFirst();
-            break;
-        case '0':
-            printf("Saliendo del programa.\n");
-            return 0;
-        default:
-            printf("Opcion no valida.\n");
+                break;
+            case '3':
+                clear_stack();
+                break;
+            case '4':
+                popFirst();
+                break;
+            case '0':
+                printf("Saliendo del programa.\n");
+                return 0;
+            default:
+                printf("Opcion no valida.\n");
         }
     }
     return 0;
